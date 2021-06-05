ebb50bb1aeeaef5b5bd8a396cf526e2307e80dca
彭群芳 <1787363729@qq.com>
2021-06-05 20:00:19 +0800

    avatar,icon,pager

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Avatar url="http://mdrs.yuanjin.tech/FgMwAPYq17So9nwVH44ltDHo7u3c" :size="100" />
+    <Icon type="home"/>
+    <Pager
+      :total="total"
+      :current="current"
+      @pageChange="handlePageChange($event)"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Avatar from './components/Avatar';
+import Icon from './components/Icon.vue';
+import Pager from './components/Pager.vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+export default{
+  data() {
+    return {
+      current: 1,
+      total: 302,
+    };
+  },
+  methods: {
+      handlePageChange(newPage) {
+        this.current = newPage;
+        console.log("加载当前页数据");
+      },
+  },
+  name:"App",
+  components:{
+    Avatar,
+    Icon,
+    Pager
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.iconfont {
+  font-size: 26px;
+  color: red;
 }
 </style>
+
+
---
+++ src/components/Icon.vue
@@ -0,0 +1,47 @@
+<template>
+  <i class="iconfont icon-container" :class="fontClass"></i>
+</template>
+    
+<script>
+const classMap = {
+    home: "icon-zhuye",
+    success: "icon-zhengque",
+    error: "icon-cuowu",
+    close: "icon-guanbi",
+    warn: "icon-jinggao",
+    info: "icon-xinxi",
+    blog: "icon-blog",
+    code: "icon-code",
+    about: "icon-set_about_hov",
+    weixin: "icon-weixin",
+    mail: "icon-email",
+    github: "icon-GitHub",
+    qq: "icon-qq",
+    arrowUp: "icon-iconfonticonfonti2copy",
+    arrowDown: "icon-tubiaozhizuo-",
+    empty: "icon-empty",
+    chat: "icon-liuyan",
+};
+
+export default {
+    props: {
+        type:{
+            type: String,
+            required: true,
+        }
+    },
+    computed: {
+        fontClass() {
+            return classMap[this.type];
+        }
+    }
+}
+</script>
+
+<style scoped>
+    @import "//at.alicdn.com/t/font_2594224_cgv1vpoa59u.css";
+    .iconfont{
+        color: inherit;
+        font-size: inherit; 
+    }
+</style>
